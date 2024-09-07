0379f2aff7f94b3fd13acbdf4bad5a5fa0dd7468
DerliJunior <contact.derli@gmail.com>
2024-09-07 09:53:50 -0300

    add base directories/configs

---
+++ src/routes/routes.jsx
@@ -0,0 +1,12 @@
+import { useQueryClient } from '@tanstack/react-query'
+import React from 'react'
+
+const Routes = () => {
+    const client = useQueryClient()
+
+    return (
+        <div>Routes</div>
+    )
+}
+
+export default Routes
